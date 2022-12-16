decfec68ed6c3fac5a18e4fd612726542df48681
Colin Smith <colin.smith@wisc.edu>
2022-12-15 16:56:52 -0800

    Save WIP

--- src/gbif_registrar/validate.py
+++ src/gbif_registrar/validate.py
@@ -4,59 +4,6 @@ from gbif_registrar.utilities import expected_cols
 import warnings
 
 
-def validate_registrations_file(file_path, extended_checks=False):
-    """Checks the registrations file for issues.
-
-    Parameters
-    ----------
-    file_path : str or pathlike object
-        Path of the registrations file.
-    extended_checks : bool
-        Run extended checks? Extended checks are repository specific.
-        Extended checks are ignored by default. They can be customized by
-        editing the source code.
-
-    Returns
-    -------
-    None
-
-    Warns
-    -----
-    Core checks
-        If registrations are incomplete.
-        If local_dataset_id values (primary keys) are not unique.
-        If local_dataset_group_id and gbif_dataset_uuid don't have 1-to-1
-        cardinality.
-        If local_dataset_id and local_dataset_endpoint don't have 1-to-1
-        cardinality.
-        If local_dataset_id has not been crawled.
-    Extended checks
-        If local_dataset_id has an invalid format.
-        If local_dataset_id has an incorrect local_dataset_group_id.
-
-    See Also
-    --------
-    initialize_registrations_file
-
-    Examples
-    --------
-    >>> validate_registrations('/Users/me/docs/registrations.csv')
-
-    With extended checks
-
-    >>> validate_registrations('/Users/me/docs/registrations.csv', extended_checks=TRUE)
-    """
-    registrations = read_registrations_file(file_path)
-    check_completeness(regs)
-    # unique primary keys
-    # Local dataset group ID and GBIF dataset uuid are 1-to-1
-    # Local dataset ID and Local dataset endpoint are 1-to-1
-    # Has been crawled
-    if extended_checks:
-        # Local dataset ID has correct format
-        # Local dataset ID has correct local dataset group ID
-
-
 def check_completeness(regs):
     """Checks for registration completeness.
 
@@ -79,9 +26,104 @@ def check_completeness(regs):
     """
     regs = regs[expected_cols()].drop(['gbif_crawl_datetime'], axis=1)
     regs = regs[regs.isna().any(axis=1)]
-    rows = regs.index.to_series() + 1
-    rows = rows.astype('string')
-    if any(rows):
+    if len(regs) > 0:
+        rows = regs.index.to_series() + 1
+        rows = rows.astype('string')
         warnings.warn('Incomplete registrations in rows: ' + ', '.join(rows))
 
 
+def check_local_dataset_id(regs):
+    """Check uniqueness of local_dataset_id (primary key)
+
+    Parameters
+    ----------
+    regs : pandas.DataFrame
+        A dataframe of the registrations file. Use`read_registrations_file` to
+        create this.
+
+    Returns
+    -------
+    None
+
+    Warns
+    -----
+    UserWarning
+        If values in the `local_dataset_id` column are not unique.
+    """
+    dupes = regs['local_dataset_id'].duplicated()
+    if any(dupes):
+        dupes = dupes.index.to_series() + 1
+        dupes = dupes.astype('string')
+        warnings.warn('Duplicate local_dataset_id values in rows: ' + ', '.join(dupes))
+
+
+def check_group_registrations(regs):
+    """Checks the local_dataset_group_id and gbif_dataset_uuid are 1-to-1.
+
+    Parameters
+    ----------
+    regs : pandas.DataFrame
+        A dataframe of the registrations file. Use`read_registrations_file` to
+        create this.
+
+    Returns
+    -------
+    None
+
+    Warns
+    -----
+    If local_dataset_group_id and gbif_dataset_uuid don't have 1-to-1
+        cardinality.
+    """
+    pass
+
+
+def validate_registrations_file(file_path, extended_checks=False):
+    """Checks the registrations file for issues.
+
+    Parameters
+    ----------
+    file_path : str or pathlike object
+        Path of the registrations file.
+    extended_checks : bool
+        Run extended checks? Extended checks are repository specific.
+        Extended checks are ignored by default. They can be customized by
+        editing the source code.
+
+    Returns
+    -------
+    None
+
+    Warns
+    -----
+    Core checks
+        If local_dataset_id and local_dataset_endpoint don't have 1-to-1
+        cardinality.
+        If local_dataset_id has not been crawled.
+    Extended checks
+        If local_dataset_id has an invalid format.
+        If local_dataset_id has an incorrect local_dataset_group_id.
+
+    See Also
+    --------
+    initialize_registrations_file
+
+    Examples
+    --------
+    >>> validate_registrations('/Users/me/docs/registrations.csv')
+
+    With extended checks
+
+    >>> validate_registrations('/Users/me/docs/registrations.csv', extended_checks=TRUE)
+    """
+    regs = read_registrations_file(file_path)
+    check_completeness(regs)
+    check_local_dataset_id(regs)
+    # Local dataset group ID and GBIF dataset uuid are 1-to-1
+    check_local_group_and_gbif_id(regs)
+    # Local dataset ID and Local dataset endpoint are 1-to-1
+    # Has been crawled
+    if extended_checks:
+        pass
+    # Local dataset ID has correct format
+    # Local dataset ID has correct local dataset group ID
--- tests/test_validate.py
+++ tests/test_validate.py
@@ -1,14 +1,27 @@
 import numpy as np
-
+import pytest
 from gbif_registrar import validate
 import warnings
 from gbif_registrar import utilities
 import pandas as pd
 
 
-def test_check_completeness_warns_when_incomplete():
-    """Incomplete registrations issue warnings."""
+@pytest.fixture
+def regs():
     regs = utilities.read_registrations_file("tests/registrations.csv")
+    return regs
+
+
+def test_check_completeness_valid(regs):
+    """The registrations file is valid, and doesn't throw a warning."""
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter('always')
+        validate.check_completeness(regs)
+        assert len(w) == 0
+
+
+def test_check_completeness_warns(regs):
+    """An empty value, in a core column, is an incomplete registration."""
     regs.loc[0, 'local_dataset_id'] = np.NaN
     regs.loc[2, 'local_dataset_endpoint'] = np.NaN
     with warnings.catch_warnings(record=True) as w:
@@ -18,11 +31,34 @@ def test_check_completeness_warns_when_incomplete():
         assert "1, 3" in str(w[0].message)
 
 
-def test_check_completeness_does_not_warn_when_complete():
-    """Complete registrations don't result in warnings."""
-    regs = utilities.read_registrations_file("tests/registrations.csv")
+def test_check_local_dataset_id_valid(regs):
+    """The registrations file is valid, and doesn't throw a warning."""
     with warnings.catch_warnings(record=True) as w:
         warnings.simplefilter('always')
-        validate.check_completeness(regs)
+        validate.check_local_dataset_id(regs)
         assert len(w) == 0
+
+
+def test_check_local_dataset_id_warns(regs):
+    """Non-unique primary keys are an issue, and accompanied by a warning."""
+    regs = pd.concat([regs, regs.iloc[[-1]]], ignore_index=True)
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter('always')
+        validate.check_local_dataset_id(regs)
+        assert "Duplicate local_dataset_id values" in str(w[0].message)
+        assert "5" in str(w[0].message)
+
+
+def test_check_group_registrations_valid(regs):
+    """The registrations file is valid, and doesn't throw a warning."""
+    with warnings.catch_warnings(record=True) as w:
+        warnings.simplefilter('always')
+        validate.check_group_registrations(regs)
+        assert len(w) == 0
+
+
+def test_check_group_registrations_warn(regs):
+    """Each local_dataset_group_id should have only one gbif_dataset_group_id
+    (and vise versa) or else a warning is issued."""
+    assert False
 
