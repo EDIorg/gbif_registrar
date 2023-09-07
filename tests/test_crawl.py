a921e165695b0a48b12981ee52ffd6d0a881a2f8
Colin Smith <colin.smith@wisc.edu>
2023-09-07 16:12:06 -0600

    refactor: reorder func params for better semantics
    Reorder the parameter positions of functions in the 'crawl' module to
    align function calls more effectively with the underlying semantics.

--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -11,5 +11,5 @@ def test_initiate_crawl():
     # # local_dataset_endpoint = "https://pasta-s.lternet.edu/package/download/eml/edi/941/4"  # Next version
     # # local_dataset_id = "edi.941.4"
     # gbif_dataset_uuid = "cfb3f6d5-ed7d-4fff-9f1b-f032ed1de485"
-    # initiate_crawl(local_dataset_endpoint, gbif_dataset_uuid, local_dataset_id)
+    # initiate_crawl(local_dataset_id, local_dataset_endpoint, gbif_dataset_uuid)
     assert True
