3b11a5d72131049a0e485938e410ef8854ea867a
Colin Smith <colin.smith@wisc.edu>
2023-10-18 15:08:04 -0700

    Validate upload_dataset works w/real requests

--- src/gbif_registrar/crawl.py
+++ src/gbif_registrar/crawl.py
@@ -2,17 +2,13 @@
 
 from time import sleep
 from gbif_registrar._utilities import (
-    _has_metadata,
     _post_new_metadata_document,
     _post_local_dataset_endpoint,
     _delete_local_dataset_endpoints,
     _read_registrations_file,
     _is_synchronized,
 )
-from gbif_registrar.config import (
-    GBIF_DATASET_BASE_URL,
-    REGISTRY_BASE_URL
-)
+from gbif_registrar.config import GBIF_DATASET_BASE_URL, REGISTRY_BASE_URL
 
 
 def upload_dataset(local_dataset_id, registrations_file):
@@ -36,6 +32,9 @@ def upload_dataset(local_dataset_id, registrations_file):
     The synchronization status of the dataset is written to the registrations
     file. The status is True if the dataset was successfully synchronized with
     GBIF and False otherwise.
+
+    Print messages indicate the progress of the upload process. The messages
+    are written to the standard output stream (stdout).
     """
     print(f"Uploading {local_dataset_id} to GBIF.")
 
@@ -46,28 +45,20 @@ def upload_dataset(local_dataset_id, registrations_file):
 
     # Stop if not registered
     if local_dataset_id not in registrations["local_dataset_id"].values:
-        print("The local dataset ID is not in the registrations file. "
-              "Registration is required first.")
+        print(
+            "The local dataset ID is not in the registrations file. "
+            "Registration is required first."
+        )
         return None
 
     # Obtain relevant information for the upload process from the registrations
-    gbif_dataset_uuid = registrations[local_dataset_id]["gbif_dataset_uuid"]
-    local_dataset_endpoint = registrations[local_dataset_id]["local_dataset_endpoint"]
-    local_dataset_group_id = registrations[local_dataset_id]["local_dataset_group_id"]
-    synchronized = registrations[local_dataset_id]["synchronized"]
-
-    # Determine if the local dataset belongs to a local data group that is
-    # already on GBIF. Do this to inform the decision logic below.
-    has_metadata = _has_metadata(gbif_dataset_uuid)
-    if has_metadata:
-        print(
-            f"The {local_dataset_group_id} dataset group exists on GBIF."
-        )
-    else:
-        print(
-            f"The {local_dataset_group_id} dataset group does not exist on "
-            f"GBIF"
-        )
+    # file.
+    index = registrations.index[
+        registrations["local_dataset_id"] == local_dataset_id
+    ].tolist()[0]
+    local_dataset_endpoint = registrations.loc[index, "local_dataset_endpoint"]
+    gbif_dataset_uuid = registrations.loc[index, "gbif_dataset_uuid"]
+    synchronized = registrations.loc[index, "synchronized"]
 
     # Check if the local_dataset_id is already synchronized with GBIF and stop
     # the upload process if it is.
@@ -80,6 +71,8 @@ def upload_dataset(local_dataset_id, registrations_file):
         # the case of a successful upload but timed out synchronization
         # check, which would result in the status being False in the
         # registrations file.
+        # FIXME: These don't index the correct data. Use the index variable
+        #  instead (declared above).
         if registrations[local_dataset_id]["synchronized"] is False:
             registrations[local_dataset_id]["synchronized"] = True
             registrations.to_csv(registrations_file, index=False, mode="w")
@@ -92,58 +85,60 @@ def upload_dataset(local_dataset_id, registrations_file):
     # Clear the list of local endpoints so when the endpoint is added below,
     # it will result in only one being listed on the GBIF dataset landing page.
     # Multiple listings could be confusing to end users.
-    if has_metadata:
-        _delete_local_dataset_endpoints(gbif_dataset_uuid)
-        print(
-            f"Deleted existing local dataset endpoints to make space for "
-            f"the new {local_dataset_id} endpoint."
-        )
+    _delete_local_dataset_endpoints(gbif_dataset_uuid)
+    print("Deleted local dataset endpoints from GBIF.")
 
     # Post the local dataset endpoint to GBIF. This will initiate a crawl of
     # the local dataset landing page metadata on the first post but not on
     # subsequent posts (updates). In the latter case, the local dataset
     # landing page metadata will also need to be posted to update the GBIF
-    # landing page.
+    # landing page (below).
     _post_local_dataset_endpoint(local_dataset_endpoint, gbif_dataset_uuid)
     print(f"Posted local dataset endpoint {local_dataset_endpoint} to GBIF.")
 
     # For revised datasets, post a new metadata document to update the GBIF
     # landing page. This is necessary because GBIF doesn't "re-crawl" the
     # local dataset metadata when the new local dataset endpoint is updated.
-    if has_metadata:
-        _post_new_metadata_document(local_dataset_id, gbif_dataset_uuid)
-        print(f"Posted new metadata document for {local_dataset_id} to GBIF.")
+    _post_new_metadata_document(local_dataset_id, gbif_dataset_uuid)
+    print(f"Posted new metadata document for {local_dataset_id} to GBIF.")
 
-    # TODO adjust max number of retries to the average synchronization time
-    #  to increase chances of success while avoiding infinite loops. But also
-    #  allow users to set the max number of retries for their system.
     # Run the is_synchronized function until a True value is returned or the
     # max number of attempts is reached.
     synchronized = False
-    max_attempts = 1
+    max_attempts = 6  # Average synchronization time is 20 seconds
     attempts = 0
     while not synchronized and attempts < max_attempts:
-        sleep(1)
         print(f"Checking if {local_dataset_id} is synchronized with GBIF.")
         synchronized = _is_synchronized(local_dataset_id, registrations_file)
         attempts += 1
+        sleep(10)
 
-    if synchronized:  # Update the registrations file with the new status
+    # Update the registrations file with the new status
+    if synchronized:
         print(f"{local_dataset_id} is synchronized with GBIF.")
         with open(registrations_file, "r", encoding="utf-8") as registrations:
             registrations = _read_registrations_file(registrations_file)
-        registrations[local_dataset_id]["synchronized"] = True
+        registrations.loc[index, "synchronized"] = True
         registrations.to_csv(registrations_file, index=False, mode="w")
         print(
             f"Updated the registrations file with the new synchronization "
             f"status of {local_dataset_id}."
         )
-        print(f"Upload of {local_dataset_id} to GBIF is complete!")
-        print("View the dataset on GBIF at:", GBIF_DATASET_BASE_URL + "/" +
-              gbif_dataset_uuid)
+        print(f"Upload of {local_dataset_id} to GBIF is complete.")
+        print(
+            "View the dataset on GBIF at:",
+            GBIF_DATASET_BASE_URL + "/" + gbif_dataset_uuid,
+        )
     else:
-        print(f"Checks on the synchronization status of {local_dataset_id} "
-              f"with GBIF timed out. Please check back later.")
-    print(f"For more information, see the GBIF log page for "
-          f"{local_dataset_id}:", REGISTRY_BASE_URL + "/" + gbif_dataset_uuid)
+        print(
+            f"Checks on the synchronization status of {local_dataset_id} "
+            f"with GBIF timed out. Please check the GBIF log page later."
+            f"Once synchronization has occured, run "
+            f"complete_registration_records function to reflect this "
+            f"update."
+        )
+    print(
+        f"For more information, see the GBIF log page for " f"{local_dataset_id}:",
+        REGISTRY_BASE_URL + "/" + gbif_dataset_uuid,
+    )
     return None
--- tests/test_crawl.py
+++ tests/test_crawl.py
@@ -9,66 +9,53 @@ from gbif_registrar.register import register_dataset
 from gbif_registrar.crawl import upload_dataset
 
 
-def test_upload_dataset():
-    """Test that the upload_dataset function works as expected."""
-    # TODO: Mock this test
-    # local_dataset_endpoint = "https://pasta-s.lternet.edu/package/download/eml/edi/941/3"
-    # local_dataset_id = "edi.941.3"
-    # # local_dataset_endpoint = "https://pasta-s.lternet.edu/package/download/eml/edi/941/4"  # Next version
-    # # local_dataset_id = "edi.941.4"
-    # gbif_dataset_uuid = "cfb3f6d5-ed7d-4fff-9f1b-f032ed1de485"
-    # upload_dataset(local_dataset_id, local_dataset_endpoint, gbif_dataset_uuid)
-    assert True
+@pytest.mark.skip(reason="Makes real HTTP requests. Run this manually.")
+def test_upload_dataset_real_requests(rgstrs, tmp_path, capsys):
+    """Test that the upload_dataset function works, for the new and updated
+     dataset cases, using real HTTP requests.
 
+    This is an integration test that actually uploads a new dataset, and then
+    revised dataset, to the GBIF staging environment from the EDI staging
+    environment."""
 
-# TODO add the capsys fixture to the function signature
-# TODO Note, for debugging, turn capsys off to see print statements. Turn it on
-#  to see the test results.
-def test_upload_dataset_real_requests(rgstrs, tmp_path):
-    """Test that the upload_dataset function works as expected using real HTTP
-    requests.
+    def test_assertions(captured):
+        """Declare test assertions for both the 'new' and 'updated' dataset
+        cases. Results are the same in both cases."""
+        # Check the std out for the expected print statements.
+        pattern = "Uploading .+ to GBIF."
+        assert search(pattern, captured.out) is not None
+        assert "Deleted local dataset endpoints" in captured.out
+        assert "Posted local dataset endpoint" in captured.out
+        assert "Posted new metadata document" in captured.out
+        pattern = "Checking if .+ is synchronized with GBIF."
+        assert search(pattern, captured.out) is not None
+        pattern = ".+ is synchronized with GBIF."
+        assert search(pattern, captured.out) is not None
+        assert "Updated the registrations file" in captured.out
+        pattern = "Upload of .+ to GBIF is complete."
+        assert search(pattern, captured.out) is not None
+        assert "View the dataset on GBIF at" in captured.out
+        assert "For more information, see " in captured.out
+        # Check that the registrations file is updated as expected.
+        registrations_final = _read_registrations_file(tmp_path / "registrations.csv")
+        assert registrations_final.loc[
+            registrations_final["local_dataset_id"] == "edi.941.3", "synchronized"
+        ].values[0]
 
-    These take a long time to run and may be flaky, but they do provide tests
-    of the real system and are therefore valuable. Real tests are done against
-    EDI staging and GBIF staging environments."""
-
-    # Set up the test:
-    # Copy the registrations file to a temporary directory for modification.
+    # Test the upload of a new dataset ...
+    # Remove records for the edi.941 group for reuse in this test
+    rgstrs = rgstrs[rgstrs["local_dataset_group_id"] != "edi.941"]
     rgstrs.to_csv(tmp_path / "registrations.csv", index=False)
-    # # Remove records for the edi.941 group for reuse in this test.
-    # registrations = _read_registrations_file(tmp_path / "registrations.csv")
-    # registrations = registrations[registrations["local_dataset_group_id"] != "edi.941"]
-    # registrations.to_csv(tmp_path / "registrations.csv", index=False)
-    # # Register edi.941.3 with GBIF.
-    # local_dataset_id_new = "edi.941.3"
-    local_dataset_id_update = "edi.941.4"
+    # Register edi.941.3 to create a new dataset group on GBIF to upload to.
+    register_dataset("edi.941.3", tmp_path / "registrations.csv")
+    # Run the upload_dataset function and assert expected results.
+    upload_dataset("edi.941.3", tmp_path / "registrations.csv")
+    captured = capsys.readouterr()
+    test_assertions(captured)
 
-    # def test_upload_dataset_new_real_requests(local_dataset_id_new, tmp_path, capsys):
-    #     """Test that the upload_dataset function works as expected for a new
-    #      dataset upload using real HTTP requests."""
-    #     upload_dataset(local_dataset_id_new, tmp_path / "registrations.csv")
-    #     assert False
-    #     # captured = capsys.readouterr()
-    #     # assert "testsss print" in captured.out
-
-    def test_upload_dataset_update_real_requests(local_dataset_id_update, tmp_path):
-        """Test that the upload_dataset function works as expected for a
-        dataset update using real HTTP requests."""
-        # TODO add ', capsys' to the function signature
-        register_dataset(local_dataset_id_update, tmp_path / "registrations.csv")
-        upload_dataset(local_dataset_id_update, tmp_path / "registrations.csv")
-        # captured = capsys.readouterr()
-        # assert "dataset group exists on GBIF." in captured.out
-        # assert "Deleted existing local dataset endpoints" in captured.out
-        # assert "Posted local dataset endpoint" in captured.out
-        # assert "Posted new metadata document" in captured.out
-        # pattern = "Checking if .+ is synchronized with GBIF."
-        # assert search(pattern, captured.out) is not None
-        # assert "For more information, see " in captured.out
-
-    # TODO add ', capsys' to the function signature
-    test_upload_dataset_update_real_requests(local_dataset_id_update, tmp_path)
-
-
-
-    assert False
+    # Now, upload an update to the edi.941 group ...
+    capsys.readouterr()  # Reset message log for this test
+    register_dataset("edi.941.4", tmp_path / "registrations.csv")
+    upload_dataset("edi.941.4", tmp_path / "registrations.csv")
+    captured = capsys.readouterr()
+    test_assertions(captured)
